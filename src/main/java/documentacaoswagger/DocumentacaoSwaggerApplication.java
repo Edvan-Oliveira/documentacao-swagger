ed156b75cde14e8186db9ac9394b067ae9ecaf1a
Edvan Oliveira <edvan.oliveiract@gmail.com>
2022-04-21 21:10:38 -0300

    Iniciado

---
+++ src/main/java/documentacaoswagger/DocumentacaoSwaggerApplication.java
@@ -0,0 +1,13 @@
+package documentacaoswagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentacaoSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentacaoSwaggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/documentacaoswagger/DocumentacaoSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package documentacaoswagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentacaoSwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
